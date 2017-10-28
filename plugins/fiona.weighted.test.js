60cc529defed4c98dbac3d85f7b7f8c0a8d91710
Billy Moon <billymoon.widows@gmail.com>
2017-10-29 00:12:58 +0100

    add weighted plugin

---
+++ plugins/fiona.weighted.test.js
@@ -0,0 +1,31 @@
+import test from 'ava'
+import fiona from '../fiona.js'
+
+test('fiona.fn.weighted (linear)', t => {
+  t.is(fiona(1).number(), 458333)
+  t.is(fiona(1).weighted('linear').number(), 458333)
+})
+
+test('fiona.fn.weighted (low)', t => {
+  t.is(fiona(1).number(), 458333)
+  t.is(fiona(1).weighted('low').number(), 6314)
+})
+
+test('fiona.fn.weighted (cube)', t => {
+  t.is(fiona(1).number(), 458333)
+  t.is(fiona(1).weighted('cube').number(), 96281)
+})
+
+test('fiona.weighted (function)', t => {
+  fiona.weighted('clamp', i => i < 0.5 ? 0.5 : i > 0.7 ? 0.7 : i)
+  t.is(typeof fiona.weighted.clamp, 'function')
+  t.is(fiona(1).number(100), 46)
+  t.is(fiona(1).weighted('clamp').number(100), 50)
+})
+
+test('fiona.weighted (array)', t => {
+  fiona.weighted('altLow', [1, 0, 1, 0])
+  t.is(typeof fiona.weighted.altLow, 'function')
+  t.is(fiona(1).number(), 458333)
+  t.is(fiona(1).weighted('altLow').number(), 6314)
+})
