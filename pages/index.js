34b55dcd9f14e8ea739c5e9126c46d059d595ad9
Billy Moon <billymoon.widows@gmail.com>
2017-10-29 21:37:44 +0000

    update docs and readme

--- pages/index.js
+++ pages/index.js
@@ -18,15 +18,14 @@ const Sample = ({ title, children, ...props }) => {
   return (
     <div {...props}>
       <pre>
-        <code>
-          <b>// {title}</b>{'\n'}
-          {code}
-        </code>
+        <code>{title && <b>// {title}</b>}{code}</code>
       </pre>
       <style jsx>{`
         float: left;
-        pre {
-          white-space: pre-wrap;
+        pre, code {
+          white-space: pre;
+          max-width: 100%;
+          overflow: scroll;
         }
       `}</style>
     </div>
@@ -61,20 +60,35 @@ export default class Main extends Component {
         <div className='centered'>
           <Logo selected={selected} className={blink && !clicked ? 'blink' : ''} clickHandler={index => evt => this.setState({ clicked: true, selected: selected === index ? Math.floor(Math.random() * 33) : index })} />
         </div>
-        {[
-          `Fiona is a (helper|tool|library) for (creating|generating) (large)? (chunks|sets) of (seeded|pseudo|mock) random data\\.`,
-          `(At it's core it uses|It is based around) a (Xorshift(256)? )?(PRNG|pseudo random number generator) that (enables|assists with|makes a mockery of) (generating|creating) (repeatable|predictable) (seemingly|apparently|authentic (feeling|looking)) random data\\.`
-        ].map((item, index) => <p key={index}>{fiona(selected).regex(item)}</p>)}
+        <p>{fiona(selected).regex(`Fiona is a (helper|tool|library) for (creating|generating) (large)? (chunks|sets) of (seeded|pseudo|mock) random data\\. (At it's core it uses|It is based around) a (Xorshift(256)? )?(PRNG|pseudo random number generator) that (enables|assists with|makes a mockery of) (generating|creating) (repeatable|predictable) (seemingly|apparently|authentic (feeling|looking)) random data\\.`)}</p>
+
+        <h2>Quickstart</h2>
+
+        <p>The most basic use case is to generate a random number: <code>{`fiona({ max: 1000000, min: 0 }).number()`}</code></p>
+
+        <Sample title='input'>{`
+        const myNumber = fiona(${selected || 952684}).number()
+        const age = fiona(${selected || 952684}).number({ max: 100 })
+        `}</Sample>
+
+        <Sample title='output'>
+          {'\n' + fiona(selected || 952684).number() +
+           '\n' + fiona(selected || 952684).number({ max: 100 })
+          }
+        </Sample>
+
+        <div className='clearfix' />
+
+        <p>It becomes more useful when creating data structures.</p>
 
         <Sample title='input'>{`
         fiona(${selected || 952684}).data({
           age: ({ seeded }) => seeded.number({ max: 100 }),
           name: ({ seeded }) => seeded.name(),
+          iban: ({ seeded }) => seeded.regex(/[A-Z]{2}\\d{2}( \\d{4}){4,5} \\d{1,3}/),
           favouriteColor: ({ seeded }) => seeded.oneOf([
             'red',
-            'pink',
-            'orange',
-            'green',
+            'yellow',
             'blue'
           ])
         })
@@ -83,9 +97,114 @@ export default class Main extends Component {
           {'\n' + JSON.stringify(fiona(selected || 952684).data({
             age: ({ seeded }) => seeded.number({ max: 100 }),
             name: ({ seeded }) => seeded.name(),
-            favouriteColor: ({ seeded }) => seeded.oneOf(['red', 'pink', 'orange', 'green', 'blue'])
+            iban: ({ seeded }) => seeded.regex(/[A-Z]{2}\d{2}( \d{4}){4,5} \d{0,3}/),
+            favouriteColor: ({ seeded }) => seeded.oneOf(['red', 'yellow', 'blue'])
           }), null, 2)}
         </Sample>
+
+        <div className='clearfix' />
+
+        <h2>Seeded pseudo random number generator</h2>
+
+        <p>At it's core, fiona has a seeded prng that will give back approximately evenly distributed floating point numbers between 0 and 1. The seed defaults to <code>{`Math.random()`}</code> can be passed in during initialisation. <code>{`fiona(seed = Math.random())`}</code></p>
+
+        <Sample title='input'>{`
+        const seeded = fiona(${selected || 952684})
+        seeded.number()
+        seeded.number()
+        `}</Sample>
+
+        <Sample title='output'>
+          {'\n// hint: change the seed in the page banner\n' + fiona(selected || 952684).number() +
+           '\n' + fiona(selected || 952684).callback(function () { this.number(); return this }).number()
+          }
+        </Sample>
+
+        <div className='clearfix' />
+
+        <p>The prng sets the initial seed when fiona is initialised, then tracks new seeds generated in consistent sequence internally. The seed can be reset to the initial value, or any arbitrary value at any time. This makes it easy to ensure data is repeatable.</p>
+
+        <Sample title='input'>{`
+        const seeded = fiona(${selected || 952684})
+        seeded.number()
+        seeded.reseed(${(selected || 952684) + 1})
+        seeded.number()
+        seeded.reseed(null) // reset seed
+        seeded.number()
+        `}</Sample>
+
+        <Sample title='output'>{(function () {
+          const out = ['']
+          const seeded = fiona(selected || 952684)
+          out.push(seeded.number())
+          seeded.reseed((selected || 952684) + 1)
+          out.push(seeded.number())
+          seeded.reseed(null)
+          out.push(seeded.number())
+          return out.join('\n\n')
+        })()}</Sample>
+
+        <div className='clearfix' />
+
+        <h2>Weighting</h2>
+
+        <p>The idea of adding a weighting to the output of random allows for powerful manipulation of large sets of pseudo random data. For example, the distribution of income is not even, where many more people are on low income than high. If we simply choose from 10,000 to 1,000,000 then the average income would be around 505,000. If we add weighting, we can make this represent our target distribution much more accurately.</p>
+
+        <Sample>{`
+        const salary = fiona(${selected || 952684}).weighting(i => i * i * i).number(100000, 10000)
+        // salary is integer between 10,000 and 100,000 but much more likely to be low
+        `}</Sample>
+
+        <div className='clearfix' />
+
+        <p>The weighting function takes a floating point number from 0-1 and returns a number from 0-1. Bezier easing functions are a nice way to shape your data.</p>
+
+        <Sample>{`
+        import bezierEasing from 'bezier-easing'
+        const salary = fiona(${selected || 952684}).weighting(bezierEasing(0.5, 1, 0, 1)).number(100000, 10000)
+        `}</Sample>
+
+        <div className='clearfix' />
+
+        <p>You can also clamp or otherwise manipulate the output with weighting functions...</p>
+
+        <Sample>{`
+        const salary = fiona(${selected || 952684}).weighting(i => i < 0.1 ? 0.1 : i).number(100000)
+        // any salary that would have been less than 10k will be 10k because the number method
+        // is based on a call to random which has been passed through the weighting function
+        `}</Sample>
+
+        <div className='clearfix' />
+
+        <h2>Chained data builder</h2>
+
+        <p>Fiona supports jQuery like chaining and extendable plugin system. There is a data builder that takes advantage of this system to assist in building complex data structures that can be modified without the existing values changing. This works by using an inline function to set the values on the data structure, which is passed a clone of the chained fiona instance seeded with the position in the data structure (the path of the function setting the value) as `seeded`.</p>
+
+        <p>There are also some core helper methods for selecting random elements from arrays for example.</p>
+
+        <Sample>{`
+        const seeded = fiona(${selected || 952684})
+        
+        // add some data to the instance
+        seeded.chain({
+          species: 'human',
+          gender: ({ seeded }) => seeded.oneOf(['Male', 'Female']),
+          planet: ({ data }) => data.gender === 'Male' ? 'Mars' : 'Venus'
+        })
+
+        // add some more data later on (extends existing data)
+        seeded.chain({
+          sentence: ({ me, seeded }) => \`I am a \${me.value().gender} from \${me.value().planet}\`
+        })
+
+        // call .value to get data
+        const plainOldObject = seeded.value()
+        `}</Sample>
+
+        <div className='clearfix' />
+
+        <p>The chained data method works by recursing and traversing objects and arrays, and any functions it finds, it will execute them, passing them the current data, a new seeded instance based on position in the data structure, the position as a path and an array builder helper.</p>
+
         <style global jsx>{`
           code {
             color: #bd10e0;
@@ -98,11 +217,14 @@ export default class Main extends Component {
             max-width: 1200px;
             margin: auto;
           }
-          p, pre {
+          p, pre, h2, h2 {
             padding: 0 15px;
           }
           .centered {
             text-align: center;
+          }
+          .clearfix {
+            clear: both;
           }
           svg {
             width: 800px;
--- readme.md
+++ readme.md
@@ -1,79 +1,23 @@
-## Quickstart
+Fiona is a tool for creating large sets of pseudo random data. At it's core it uses a Xorshift256 PRNG that makes a mockery of creating predictable seemingly random data.
 
-The most basic use case is to generate a random number: `fiona().number(max=1000000, min=0)`
+## Quickstart
 
-    const myNumber = fiona().number()
-    // myNumber is an integer between 1 and 1,000,000
-    const age = fiona().number(100)
-    // age is integer between 0 and 100
+The most basic use case is to generate a random number: `fiona({ max: 1000000, min: 0 }).number()`
+
+    // input                                      // output
+    const myNumber = fiona(24).number()           553925
+    const age = fiona(24).number({ max: 100 })    55
 
 It becomes more useful when creating data structures.
 
-    const data = fiona()
-
-    const temperatureChange = {
-      summer: data.number(15, 30),
-      autumn: data.number(10, 25),
-      winter: data.number(5, 20),
-      spring: data.number(10, 25)
-    }
-
-### Seeded pseudo random number generator
-
-At it's core, fiona has a seeded prng that will give back approximately evenly distributed floating point numbers between 0 and 1. The seed defaults to `Math.random()` can be passed in during initialisation. `fiona(seed=Math.random())`
-
-    const data = fiona(12345)
-    data.number() // always the same integer
-    data.number() // always the same integer, but different from the first
-
-The prng sets the initial seed when fiona is initialised, then tracks new seeds generated in consistent sequence internally. The seed can be reset to the initial value, or any arbitrary value at any time. This makes it easy to ensure data is repeatable.
-
-    const data = fiona(12345)
-    data.number() // seeded with 12345
-    data.seed(67890)
-    data.number() // seeded with 67890
-    data.seed(null) // reset seed to initial value
-    data.number() // seeded with 12345
-
-### Weighting
-
-The idea of adding a weighting to the output of random allows for powerful manipulation of large sets of pseudo random data. For example, the distribution of income is not even, where many more people are on low income than high. If we simply choose from 10,000 to 1,000,000 then the average income would be around 505,000. If we add weighting, we can make this represent our target distribution much more accurately.
-
-    const salary = fiona().weighting(i => i * i * i).number(100000, 10000)
-    // salary is integer between 10,000 and 100,000 but much more likely to be low
-    
-The weighting function takes a floating point number from 0-1 and returns a number from 0-1. Bezier easing functions are a nice way to shape your data.
-
-    import bezierEasing from 'bezier-easing'
-    const salary = fiona().weighting(bezierEasing(0.5, 1, 0, 1)).number(100000, 10000)
-    
-You can also clamp or otherwise manipulate the output with weighting functionis...
-
-    const salary = fiona().weighting(i => i < 0.1 ? 0.1 : i).number(100000)
-    // any salary that would have been less than 10k will be 10k because the number method
-    // is based on a call to random which has been passed through the weighting function
-
-### Chained data builder
-
-Fiona supports jQuery like chaining and extendable plugin system. There is a data builder that takes advantage of this system to assist in building complex data structures that can be modified without the existing values changing. This works by using an inline function to set the values on the data structure, which is passed a clone of the chained fiona instance seeded with the position in the data structure (the path of the function setting the value) as `seeded`.
-
-There are also some core helper methods for selecting random elements from arrays for example.
-
-    const seeded = fiona(12345)
-    
-    // add some data to the instance
-    seeded.data({
-      species: 'human',
-      gender: ({ seeded }) => seeded.oneOf(['Male', 'Female']),
-      planet: ({ data }) => data.gender === 'Male' ? 'Mars' : 'Venus'
-    })
-    
-    // add some more data later on (extends existing data)
-    seeded.data({
-      sentence: ({ me, seeded }) => `There are ${me.data().bottles} ${seeded.oneOf(['red', 'green', 'blue'])} bottles on the wall`
-    })
-    
-    // call .data without arguments to get data
-    const plainOldObject = seeded.data()
-
-The chained data method works by recursing and traversing objects and arrays, and any functions it finds, it will execute them, passing them the current data, a new seeded instance based on position in the data structure, the position as a path and an array builder helper.
+    // input                                                                       // output
+    fiona(24).data({                                                               {
+      age: ({ seeded }) => seeded.number({ max: 100 }),                              "age": 88,
+      name: ({ seeded }) => seeded.name(),                                           "name": "Dr Poppy MacDonald",
+      iban: ({ seeded }) => seeded.regex(/[A-Z]{2}\d{2}( \d{4}){4,5} \d{1,3}/),      "iban": "VR70 9829 1916 5836 9013 6470 86",
+      favouriteColor: ({ seeded }) => seeded.oneOf([                                 "favouriteColor": "yellow"
+        'red',                                                                     }
+        'yellow',                                                                 
+        'blue'                                                                    
+      ])                                                                          
+    })                                                                            
