04ff542125f5c003df6ee66c2cdc4114ce85da95
billy moon <billy.moon@wipro.com>
2018-05-18 00:42:26 +0100

    add missing json plugin files

---
+++ src/plugins/fiona.json.test.js
@@ -0,0 +1,15 @@
+/* global test,expect */
+
+const fiona = require('../')
+require('./fiona.json')
+
+test('fiona.fn.json', () => {
+  expect(fiona(1).chain(1).json()).toBe('1')
+  expect(fiona(1).chain('1').json()).toBe('"1"')
+  expect(fiona(1).chain({ num: ({ seeded }) => seeded.number(), str: 'value' }).json()).toBe('{"num":507868,"str":"value"}')
+  expect(fiona(1).chain([1, null, "str", {key: 'val'}]).json()).toBe('[1,null,"str",{"key":"val"}]')
+})
+
+test('JSON.stringify(seeded)', () => {
+  expect(JSON.stringify(fiona(1).chain([1, null, "str", {key: 'val'}]))).toBe('[1,null,"str",{"key":"val"}]')
+})
