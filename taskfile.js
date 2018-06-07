1ec06d70985ca00a3459bcd86a52dfcd6b19014f
billy moon <billy.moon@wipro.com>
2018-06-07 12:01:03 +0100

    start using husky and taskr

---
+++ taskfile.js
@@ -0,0 +1,52 @@
+// node core deps
+const childProcess = require('child_process')
+
+// 3rd party deps
+const Repo = require('git-tools')
+
+/* utils */
+
+const exec = cmd => childProcess.execSync(cmd, { encoding: 'utf8', stdio: ['inherit', 'inherit', 'inherit'] })
+
+const repo = new Repo()
+const git = method => new Promise(resolve => repo[method]((err, value) => resolve(value)))
+
+/* tasks */
+
+export async function deployMaster (task) {
+  const branch = await git('currentBranch')
+  if (branch === 'master') {
+    task.start('deploy')
+  }
+}
+
+export async function reports (task) {
+  task.start('coverage')
+}
+
+export async function coverage (task) {
+  exec('jest --coverage')
+  task.clear('static/reports/coverage').source('coverage/lcov-report/**').target('static/reports/coverage')
+}
+
+export async function build (task) {
+  task.parallel(['fiona', 'docs', 'core'])
+  task.start('reports')
+}
+
+export async function fiona (task) {
+  exec('webpack')
+}
+
+export async function docs (task) {
+  exec('next build')
+}
+
+export async function core (task) {
+  exec('webpack --config webpack.config.core.js')
+}
+
+export async function deploy (task) {
+  exec(`now alias $(now) fiona`)
+  task.$.log('deployed to: https://fiona.now.sh')
+}
