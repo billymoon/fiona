81b030aaf278568a55afb46d77350e5d68f558d8
billy moon <billy.moon@wipro.com>
2018-06-07 02:53:20 +0100

    fix api overview seed and improve npm tasks

--- src/docs/sections/api-overview.js
+++ src/docs/sections/api-overview.js
@@ -1,6 +1,10 @@
-export default () =>
+import { injectState } from '../app'
+
+const Section = ({ state: { seed } }) =>
   <section>
     <h2>API</h2>
     <p>Fiona has a jQuery like plugin architecture where methods are attached to <code>fiona.fn</code> to operate on an instance, and directly on <code>fiona</code> when used as utility.</p>
-    <p>For demonstration purposes, a seed of <code>24</code> is used, but changing this should render different, but consistent results.</p>
+    <p>For demonstration purposes, a seed of <code>{seed}</code> is used, but changing this should render different, but consistent results.</p>
   </section>
+
+export default injectState(Section)
