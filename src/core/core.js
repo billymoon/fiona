1c179a3aca3db8138658426d47ff6be85bc493d0
billy moon <billy.moon@wipro.com>
2018-06-15 02:36:44 +0100

    bump

--- src/core/core.js
+++ src/core/core.js
@@ -79,7 +79,6 @@ const fiona = (...args) => new Moon(...args)
 
 // TODO: don't reuquire whole package json just to get version string
 fiona.version = packageJson.version
-fiona.description = packageJson.description
 
 // set up self referencial prototype chain with jQuery like plugin architecture
 fiona.fn = Moon.prototype = { constructor: Moon }
