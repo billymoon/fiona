07bc6f78ba4e841390e06b9c391545902407abf0
Billy Moon <billy@itaccess.org>
2018-08-24 14:02:51 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const config = require('./webpack.config.core')
 
-config.entry.unshift('./src/plugins/index.js')
+config.entry = ['./kitchen/index.js']
 config.output.filename = 'fiona.min.js'
 
 module.exports = config
